e3a7fd7cc7eda280906caf32c62c8840a8601140
Jens Panis <43874435+JensPanisPXL@users.noreply.github.com>
2020-12-15 12:37:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
             <button type="submit" class="btn btn-primary" name="add">Add</button>
             <button type="submit" class="btn btn-primary" name="subtract">Subtract</button>
             <button type="submit" class="btn btn-primary" name="multiply">Multiply</button>
-            <button type="submit" class="btn btn-primary" name="multiply">division</button>
+            <button type="submit" class="btn btn-primary" name="division">division</button>
         </form>
         <main>
         <?php
